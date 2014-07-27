a0f93623c6086bfefee15eefd3170594df68b271
Shelvacu <shelvacu@gmail.com>
2014-07-27 02:32:01 +0000

    switching to C language

--- weather.py
+++
@@ -1,7 +0,0 @@
-import urllib
-import json
-base_url = "http://api.openweathermap.org/data/2.5/"
-
-def get_weather(city):
-    "Queries openweathermap for the given city/location. Returns JSON"
-    return json.loads(urllib.request.urlopen(base_url+"weather?q="+city).read())
